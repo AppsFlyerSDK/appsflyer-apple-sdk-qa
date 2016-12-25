823d0848d51da05458f0faa2b013e33dfb3e34c8
maxim@appsflyer.com <maxim@appsflyer.com>
2016-12-25 10:04:20 +0200

    Jenkins auto build

--- AppsFlyerTracker.framework/AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
+++ AppsFlyerTracker.framework/AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 1.2.130
+//  AppsFlyer iOS SDK 1.2.131
 //  Copyright (c) 2013 AppsFlyer Ltd. All rights reserved.
 //
 
@@ -80,7 +80,8 @@
 typedef enum  {
     EmailCryptTypeNone = 0,
     EmailCryptTypeSHA1 = 1,
-    EmailCryptTypeMD5 = 2
+    EmailCryptTypeMD5 = 2,
+    EmailCryptTypeSHA256 = 3
 } EmailCryptType;
 
 /*
