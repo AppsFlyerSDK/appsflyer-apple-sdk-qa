36ed976e8fb37efa98737e4c624f08b3548ec34f
Maxim Shoustin <maxim@appsflyer.com>
2016-12-28 15:24:51 +0200

    Jenkins auto build

--- AppsFlyerTracker.framework/AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
+++ AppsFlyerTracker.framework/AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 1.2.150
+//  AppsFlyer iOS SDK 1.2.160
 //  Copyright (c) 2013 AppsFlyer Ltd. All rights reserved.
 //
 
@@ -80,8 +80,7 @@
 typedef enum  {
     EmailCryptTypeNone = 0,
     EmailCryptTypeSHA1 = 1,
-    EmailCryptTypeMD5 = 2,
-    EmailCryptTypeSHA256 = 3
+    EmailCryptTypeMD5 = 2
 } EmailCryptType;
 
 /*
@@ -193,7 +192,7 @@ typedef enum  {
  * Example :
  *      [[AppsFlyer sharedTracker] trackEvent:@"hotel-booked" withValue:"200"];
  */
-- (void) trackEvent:(NSString*)eventName withValue:(NSString*)value __attribute__((deprecated));
+- (void) trackEvent:(NSString*)eventName withValue:(NSString*)value;
 
 /*
  * Use this method to track an events with mulitple values. See AppsFlyer's documentation for details. 
