64e0f05a6aa629a8232b625891845c491fdcfd71
af_deployment <devop@appsflyer.com>
2018-12-03 15:56:01 +0200

    Jenkins auto build

--- AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
+++ AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 9.9.763 (763)
+//  AppsFlyer iOS SDK 9.9.764 (764)
 //  Copyright (c) 2018 AppsFlyer Ltd. All rights reserved.
 //
 
