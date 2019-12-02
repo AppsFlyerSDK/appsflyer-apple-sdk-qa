1fdf0e910e9a3ccabfb68e0bbc7998ccfa586ebb
Andrii Hahan <andrii.h@appsflyer.com>
2019-12-02 16:11:47 +0200

    Jenkins auto build

--- Mac Catalyst/AppsFlyerLib.xcframework/ios-x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
+++ Mac Catalyst/AppsFlyerLib.xcframework/ios-x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 9.9.931 (931)
+//  AppsFlyer iOS SDK 9.9.932 (932)
 //  Copyright (c) 2012-2019 AppsFlyer Ltd. All rights reserved.
 //
 
