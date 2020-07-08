1b5933145bcb7f57df479a0ad1125b5da153d9e1
Maxim <maxim@appsflyer.com>
2020-07-08 12:25:36 +0300

    Jenkins auto build

--- Mac Catalyst/AppsFlyerLib.xcframework/ios-i386_x86_64-simulator/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
+++ Mac Catalyst/AppsFlyerLib.xcframework/ios-i386_x86_64-simulator/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 5.4.2.147 (147)
+//  AppsFlyer iOS SDK 5.4.2.148 (148)
 //  Copyright (c) 2012-2019 AppsFlyer Ltd. All rights reserved.
 //
 
