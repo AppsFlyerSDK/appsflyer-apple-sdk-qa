7e4e5139650036d0fe34eee767fea036fd901978
Maxim <maxim@appsflyer.com>
2020-10-04 17:35:18 +0300

    Jenkins auto build

--- tvOS/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
+++ tvOS/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.0.5.190 (190)
+//  AppsFlyer iOS SDK 6.0.5.191 (191)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
