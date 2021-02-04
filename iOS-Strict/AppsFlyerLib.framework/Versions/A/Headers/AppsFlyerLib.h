62451c281dd060cc2b74ebe9922b3cd96644d380
Maxim <maxim@appsflyer.com>
2021-02-04 20:07:51 +0200

    Jenkins auto build

--- iOS-Strict/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
+++ iOS-Strict/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.2.0.54 (54)
+//  AppsFlyer iOS SDK 6.2.0.55 (55)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
