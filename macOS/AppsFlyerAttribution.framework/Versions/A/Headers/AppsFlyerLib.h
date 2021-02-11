cb500af14026500dbbd0be09e9e1fbe7994ee2ca
Maxim <maxim@appsflyer.com>
2021-02-11 17:04:07 +0200

    Jenkins auto build

--- macOS/AppsFlyerAttribution.framework/Versions/A/Headers/AppsFlyerLib.h
+++ macOS/AppsFlyerAttribution.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.2.1.59 (59)
+//  AppsFlyer iOS SDK 6.2.1.60 (60)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
