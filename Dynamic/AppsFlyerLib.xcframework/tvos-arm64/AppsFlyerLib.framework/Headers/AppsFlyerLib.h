05b9eaf3d6c11c9e668576d45b989808b408097d
amit.kremer <amit.kremer@appsflyer.com>
2022-01-31 10:24:46 +0200

    Jenkins auto build

--- Dynamic/AppsFlyerLib.xcframework/tvos-arm64/AppsFlyerLib.framework/Headers/AppsFlyerLib.h
+++ Dynamic/AppsFlyerLib.xcframework/tvos-arm64/AppsFlyerLib.framework/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.5.2.54 (54)
+//  AppsFlyer iOS SDK 6.5.3.56 (56)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
