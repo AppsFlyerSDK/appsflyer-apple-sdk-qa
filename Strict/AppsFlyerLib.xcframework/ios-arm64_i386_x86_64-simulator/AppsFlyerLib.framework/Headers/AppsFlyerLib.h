d8cb996f010b420650df169820cd1536a31127cd
wesfieldj <jonathan.wesfield@appsflyer.com>
2022-02-21 11:15:34 +0200

    Jenkins auto build

--- Strict/AppsFlyerLib.xcframework/ios-arm64_i386_x86_64-simulator/AppsFlyerLib.framework/Headers/AppsFlyerLib.h
+++ Strict/AppsFlyerLib.xcframework/ios-arm64_i386_x86_64-simulator/AppsFlyerLib.framework/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.5.3.57 (57)
+//  AppsFlyer iOS SDK 6.5.3.58 (58)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
