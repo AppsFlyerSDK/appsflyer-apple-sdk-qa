e686b5f05adf1ca800cd13c456938c209a79e339
amit.kremer <amit.kremer@appsflyer.com>
2022-06-09 20:34:20 +0300

    Jenkins auto build

--- AppsFlyerLib.xcframework/ios-arm64_x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
+++ AppsFlyerLib.xcframework/ios-arm64_x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.7.0.79 (79)
+//  AppsFlyer iOS SDK 6.7.0.80 (80)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
