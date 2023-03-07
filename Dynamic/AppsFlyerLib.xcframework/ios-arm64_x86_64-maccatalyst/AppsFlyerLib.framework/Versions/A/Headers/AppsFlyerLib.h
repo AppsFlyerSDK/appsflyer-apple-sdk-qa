9eaadd8583a9ba20fb7da8a0d4c2091bacc34d43
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-03-07 17:14:34 +0200

    Jenkins auto build

--- Dynamic/AppsFlyerLib.xcframework/ios-arm64_x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
+++ Dynamic/AppsFlyerLib.xcframework/ios-arm64_x86_64-maccatalyst/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.10.1.105 (105)
+//  AppsFlyer iOS SDK 6.10.1.106 (106)
 //  Copyright (c) 2012-2023 AppsFlyer Ltd. All rights reserved.
 //
 
