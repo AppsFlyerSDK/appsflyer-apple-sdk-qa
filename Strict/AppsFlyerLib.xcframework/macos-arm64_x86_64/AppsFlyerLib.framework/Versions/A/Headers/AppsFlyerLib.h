59637d180cde486b71c50d7374b2ae7d55cea0d0
veronica.b <veronica.b@appsflyer.com>
2023-05-03 16:45:46 +0300

    Jenkins auto build

--- Strict/AppsFlyerLib.xcframework/macos-arm64_x86_64/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
+++ Strict/AppsFlyerLib.xcframework/macos-arm64_x86_64/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.11.1.115 (115)
+//  AppsFlyer iOS SDK 6.11.1.117 (117)
 //  Copyright (c) 2012-2023 AppsFlyer Ltd. All rights reserved.
 //
 
