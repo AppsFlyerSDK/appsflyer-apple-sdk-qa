9842d1b64b6c715e8225ac6d3c785d6d9efa5283
Amit kremer <amit.appsflyer@appsflyer.com>
2024-03-28 13:26:52 +0200

    Podspec, Package.swift file and Carthage json files have been updated to 6.14.0.151

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.13.2.160/AppsFlyerLib-Static-SPM.xcframework.zip",
-            checksum: "9dd5898ea9ee0ec63fff5d9912dffd28d77c5cd7c8f55bc2ccce1883a946d811"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.14.0.151/AppsFlyerLib-Static-SPM.xcframework.zip",
+            checksum: "4bf5caa120517ef845ec6d6dad382071059085b4ac2ee507365166c0ed644841"
         )
     ]
 )
