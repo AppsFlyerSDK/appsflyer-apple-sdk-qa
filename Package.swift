bcd183c49bfdb9f39cd23b8b50538e47732fe4e5
ec2-user <ec2-user@ip-10-10-86-122.eu-west-1.compute.internal>
2026-05-07 10:31:24 +0000

    Podspec, Package.swift file and Carthage json files have been updated to 7.0.0.35701110

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/7.0.0.35694705/AppsFlyerLib-Static-SPM.xcframework.zip",
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/7.0.0.35701110/AppsFlyerLib-Static-SPM.xcframework.zip",
             checksum: "55c238fbff601ad9eebd149a2b27ed303e61328639d3ccd25636166a7461cc07"
         )
     ]
